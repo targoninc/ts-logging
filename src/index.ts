20a4e77f83c29ddc819210a90d133ef88c61daef
alexa <alex@targoninc.com>
2025-05-01 16:40:47 +0200

    change log level export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export {CLI, stack, configureDBLogging} from "./CLI";
+export {LogLevel} from "./defs/LogLevel";
 export {IP} from "./IP";
 
 export type {LoggingDb} from "./defs/LoggingDb";
 export type {LogConfig} from "./defs/LogConfig";
-export type {LogLevel} from "./defs/LogLevel";
